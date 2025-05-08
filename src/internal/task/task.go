f9440fd401602986f4f07abccd0df3b076dcc73b
Harpreet Singh <harpreetsinghsandhu021@gmail.com>
2025-05-08 14:13:34 +0530

    Initial commit: Basic structure

---
+++ src/internal/task/task.go
@@ -0,0 +1,41 @@
+package task
+
+import (
+	"time"
+
+	"github.com/docker/go-connections/nat"
+	"github.com/google/uuid"
+)
+
+type State int
+
+const (
+	Pending State = iota
+	Scheduled
+	Running
+	Completed
+	Failed
+)
+
+// Represents a task that a user wants to run on our cluster.
+type Task struct {
+	ID            uuid.UUID
+	Name          string
+	State         State
+	Image         string
+	Memory        int
+	Disk          int
+	ExposedPorts  nat.PortSet
+	PortBindings  map[string]string
+	RestartPolicy string
+	StartTime     time.Time
+	FinishTime    time.Time
+}
+
+// Represents a task that needs to be stopped.
+type TaskEvent struct {
+	ID        uuid.UUID
+	State     State
+	Timestamp time.Time
+	Task      Task
+}
---
+++ src/internal/worker/worker.go
@@ -0,0 +1,37 @@
+package worker
+
+import (
+	"fmt"
+	"litekube/src/internal/task"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+// Represents a worker in the orchestrator with these functions:
+// 1. Run tasks as Docker containers
+// 2. Accept tasks to run from a manager
+// 3. Provide relevant statistics to the manager for the purpose of scheduling tasks
+// 4. Keep track of its tasks and their state
+type Worker struct {
+	Name      string
+	Queue     queue.Queue
+	Db        map[uuid.UUID]*task.Task
+	TaskCount int
+}
+
+func (w *Worker) RunTask() {
+	fmt.Println("I will start or stop a task")
+}
+
+func (w *Worker) CollectStats() {
+	fmt.Println("I will collect stats")
+}
+
+func (w *Worker) StartTask() {
+	fmt.Println("I will start a task")
+}
+
+func (w *Worker) StopTask() {
+	fmt.Println("I will stop a task")
+}
